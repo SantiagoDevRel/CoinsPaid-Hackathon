a1b423f86deb7c7c0aa7a1e116bfd968dcc3f42d
santiagodevrel <santiago@chainsafe.io>
2024-04-20 16:30:29 +0300

    update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [balanceAvailableToWithdraw, setBalanceAvailableToWithdraw] = useState("0x");
   const [balanceOfBizWallet, setBalanceOfBizWallet] = useState("0x");
+  const [clientAddresses, setClientAddresses] = useState([]);
   const [wallet, setWallet] = useState("0x");
   let web3, accounts, contract;
 
@@ -13,17 +14,39 @@ function App() {
   async function connect() {
     web3 = new Web3(window.ethereum);
     accounts = await web3.eth.requestAccounts();
+
+    //TODO
+    //initialize contract
     //contract = new web3.eth.Contract(ABI, ADDRESS)
   }
 
   async function updateBalanceAvailableToWithdraw() {
+    await connect();
+    //TODO
     //run a for loop over the belonging addresses to this business with a `getBalance()`
     //so we can get the total balance belonging to this business
+    let balanceAvailable = 0;
+    for (let i = 0; i < clientAddresses.length; i++) {
+      const balance = Number(await web3.eth.getBalance(clientAddresses[i]));
+      balanceAvailable = balance + balanceAvailable;
+    }
+    setBalanceAvailableToWithdraw(String(balanceAvailable));
   }
 
-  async function createAddress() {}
+  async function createAddress() {
+    await connect();
+    const newAccount = web3.eth.accounts.create();
+    //TODO
+    //somehow store the PK of this accounts in the plugin or somewhere
+    //so coinspaid is able to manage this accounts
+    setClientAddresses((prevAddresses) => [...prevAddresses, newAccount.address]);
+    console.log(clientAddresses);
+  }
 
-  async function withdraw() {}
+  async function withdraw() {
+    //TODO
+    //send funds from the client addresses to the business
+  }
 
   async function updateCurrentBalanceOnChain() {
     await connect();
@@ -35,13 +58,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={updateBalanceAvailableToWithdraw}>Update Balance Available to Withdraw</button>
-        <button onClick={updateCurrentBalanceOnChain}>Update Balance Of Biz Wallet</button>
-        <button onClick={withdraw}>Withdraw Funds</button>
-        <button onClick={createAddress}>Generate new address</button>
-        <button onClick={withdraw}>Withdraw Funds</button>
-        <p>Balance available to withdraw: {balanceAvailableToWithdraw}</p>
-        <p>Balance of business wallet: {balanceOfBizWallet}</p>
+        <button className="button" onClick={updateBalanceAvailableToWithdraw}>
+          Update Balance Available to Withdraw
+        </button>
+        <button className="button" onClick={updateCurrentBalanceOnChain}>
+          Update Balance Of Biz Wallet
+        </button>
+        <button className="button" onClick={withdraw}>
+          Withdraw Funds
+        </button>
+        <p className="balance-info">Balance available to withdraw: {balanceAvailableToWithdraw}</p>
+        <p className="balance-info">Balance of business wallet: {balanceOfBizWallet}</p>
+        <button className="button" onClick={createAddress}>
+          Generate new address
+        </button>
+        <ul className="address-list">
+          {clientAddresses.map((address, index) => (
+            <li key={index}>{address}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
